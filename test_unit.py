c2f2f5c87afd3afd69ff45d8edb48f496f408b3c
sadeelanjum <sadeel094@gmail.com>
2018-01-01 15:04:18 +0500

    Update test_unit.py

--- test_unit.py
+++ test_unit.py
@@ -4,5 +4,5 @@ def test_adding_module():
 	sum = code.add_numbers(12,8)
 	assert sum == 20
 def test_mutiply_module():
-	sum = code.multiply_numbers(12,10)
-	assert sum == 120
+	sum = code.multiply_numbers(2,10)
+	assert sum == 20
